43d1e1a9681344518870e53083932ec121f61a2f
Cole Nicholson <koster.nicholson@gmail.com>
2026-08-30 17:29:23 -0700

    Implemented Partner Split

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import sys
 import pandas as pd
 
-# goals:
-# how much revenue from each country
-# how much revenue 
-# how much profit (51% of revenue after ad_spend is subtracted)
-
-
 def main():
     file = sys.argv[1]
     #ad_spend = sys.argv[2]
-    
 
     try:
         df = pd.read_csv(file) 
@@ -18,6 +11,7 @@ def main():
         print(f"{df.groupby("Card Address Country")["Total Spend"].sum().sort_values(ascending=False).rename_axis(None).to_string()}")
         print(f"Revenue: ${df['Total Spend'].sum():.2f}")
         print(f"Profit: ${df['Total Spend'].sum() * .51:.2f}")
+        print(f"Partner Share: ${df['Total Spend'].sum() * .49:.2f}")
         
                        
     except FileNotFoundError:
